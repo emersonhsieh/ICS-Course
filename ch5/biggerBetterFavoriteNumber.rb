a13599acaecce9e07b8a3cbafe09ab487d466c21
Emerson Hsieh <emersonhsieh@berkeley.edu>
2017-06-01 21:06:03 -0700

    Added ch5

---
+++ ch5/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,4 @@
+puts "What is your favorite number?"
+num = gets.chomp
+bnum = num.to_i + 1
+puts bnum.to_s + " is a bigger number, therefore it is your new favorite number."
---
+++ ch5/fullNameGreeting.rb
@@ -0,0 +1,8 @@
+puts "Hello! What is your first name?"
+fn = gets.chomp
+puts "What is your middle name?"
+mn = gets.chomp
+puts "What is your last name?"
+ln = gets.chomp
+
+puts "Welcome, " + fn + " " + mn + " " + ln
