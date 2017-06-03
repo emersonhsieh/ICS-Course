084e499b76a54c74769eebec6dbfa75114f6304e
Emerson Hsieh <emersonhsieh@berkeley.edu>
2017-06-02 21:42:56 -0700

    Added Chapter 6

---
+++ ch6/angryBoss.rb
@@ -0,0 +1,4 @@
+puts "Boss: What do you want?"
+m = gets.chomp
+
+puts "WHADDAYA MEAN \"" + m.upcase + "\"?!? YOU'RE FIRED!!"
---
+++ ch6/tableOfContents.rb
@@ -0,0 +1,11 @@
+line_width = 100
+
+c1 = "Chapter 1: Getting Started"
+c2 = "Chapter 2: Numbers"
+c3 = "Chapter 3: Letters"
+
+puts("Table of Contents".center(107*0.5))
+puts(c1.ljust(line_width*0.5) + "page 1 ".ljust(line_width*0.5))
+puts(c2.ljust(line_width*0.5) + "page 9 ".ljust(line_width*0.5))
+puts(c3.ljust(line_width*0.5) + "page 13".ljust(line_width*0.5))
+
